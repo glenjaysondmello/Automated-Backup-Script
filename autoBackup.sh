45853bb1c8e2320f9cb50593f9c8d116179bc207
Glen Jayson Dmello <glendmello04@gmail.com>
2025-07-15 23:32:13 +0530

    added log file to show log messages

--- autoBackup.sh
+++ autoBackup.sh
@@ -2,13 +2,21 @@
 
 SOURCE_DIR="$HOME/Documents"
 BACKUP_DIR="$HOME/AutoBackups/backups"
+LOG_FILE="$HOME/AutoBackups/backup.log"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 BACKUP_FILE="backup_$TIMESTAMP.tar.gz"
 
 mkdir -p "$BACKUP_DIR"
+touch "$LOG_FILE"
 
 tar -czvf "$BACKUP_DIR/$BACKUP_FILE" -C "$SOURCE_DIR" .
 
-echo "Backup Completed: $BACKUP_DIR/$BACKUP_FILE"
+if [ $? -eq 0 ]; then
+    echo "$(date +"%Y-%m-%d %H-%M-%S") Backup Successful: $BACKUP_FILE" >>"$LOG_FILE"
+    echo "Backup completed: $BACKUP_FILE"
+else
+    echo "$(date +"%Y-%m-%d %H-%M-%S") Backup Failed: $BACKUP_FILE" >>"$LOG_FILE"
+    echo "Backup failed"
+fi
--- restore.sh
+++ restore.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 BACKUP_DIR="$HOME/AutoBackups/backups"
+LOG_FILE="$HOME/AutoBackups/backup.log"
 
 echo "Available Backups: "
-ls -l "$BACKUP_DIR"
+ls -lh "$BACKUP_DIR"
 
 read -p "Enter the Backup File to Restore: " BACKUP_FILE
 
@@ -11,7 +12,14 @@ read -p "Enter the Restore Location (default: $HOME/RestoredFiles): " RESTORE_DI
 RESTORE_DIR=${RESTORE_DIR:-"$HOME/RestoredFiles"}
 
 mkdir -p "$RESTORE_DIR"
+touch "$LOG_FILE"
 
 tar -xzvf "$BACKUP_DIR/$BACKUP_FILE" -C "$RESTORE_DIR"
 
-echo "Backup restored to: $RESTORE_DIR"
+if [ $? -eq 0 ]; then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Restore successful: $BACKUP_FILE to $RESTORE_DIR" >>"$LOG_FILE"
+    echo "Backup restored to: $RESTORE_DIR"
+else
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Restore failed: $BACKUP_FILE" >>"$LOG_FILE"
+    echo "Restore failed"
+fi
